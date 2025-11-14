da60fbc265d657efa23715dfb96c857bebdb28d2
Nazmul Hoque <nazmul.info.duet@gmail.com>
2025-11-14 20:38:14 +0600

    Implement post and get methods

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 3000;
-//Ux9VTXIDC8yWSBWm
-//ArtCollection
+const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion } = require("mongodb");
+//middleware
+app.use(cors());
+app.use(express.json());
+//kzS3Lzr8kLeDepUz
+//ArtworkDatabaseUser
 const uri =
-  "mongodb+srv://ArtCollection:Ux9VTXIDC8yWSBWm@cluster0.phwuzwa.mongodb.net/?appName=Cluster0";
+  "mongodb+srv://ArtworkDatabaseUser:kzS3Lzr8kLeDepUz@cluster0.wsnudbo.mongodb.net/?appName=Cluster0";
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -19,6 +23,27 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const artworkDB = client.db("artworkDB");
+    const artworkCollection = artworkDB.collection("artwork");
+    // POST route to receive form data
+    app.post("/artworks", async (req, res) => {
+      const formData = req.body; // Get data from client
+      console.log("Received Data:", formData);
+      const result = await artworkCollection.insertOne(formData);
+      // Send response to client
+      res.send(result);
+    });
+    //get data database to server
+    app.get("/artworks", async (req, res) => {
+      try{
+        const result = await artworkCollection.find({}).toArray();
+        res.send(result);
+      }
+      catch(err){
+        res.status(500).send({error:err.message});
+      }
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -26,13 +51,10 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
